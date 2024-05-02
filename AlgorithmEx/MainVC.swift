473ecc410c4f6b6a363350af7e7725d1c6437cfd
techie262 <techie262@gmail.com>
2024-05-02 11:47:15 +0700

    init

---
+++ AlgorithmEx/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  LetCode
+//
+//  Created by Bigbear on 25/4/24.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        run()
+    }
+    
+    func run() {
+        Solution().run()
+    }
+
+}
---
+++ LetCode/Tree/938. Range Sum of BST.swift
@@ -0,0 +1,44 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        var sum = 0
+        
+        if root.val >= low && root.val <= high {
+            sum += root.val
+        }
+        
+        if root.val > low {
+            sum += rangeSumBST(root.left, low, high)
+        }
+        
+        if root.val < high {
+            sum += rangeSumBST(root.right, low, high)
+        }
+        
+        return sum
+    }
+    
+    func run() {
+        let root = TreeNode(10, TreeNode(5, TreeNode(3), TreeNode(7)), TreeNode(15, nil, TreeNode(18)))
+        let solution = Solution()
+        print(solution.rangeSumBST(root, 7, 15)) // Kết quả mong đợi: 32
+    }
+    
+}
+
+
